169be4ade8b0ea5dd22f5fc085b64301a30de678
spgiri808-cpu <spgiri808@gmail.com>
2026-06-04 20:31:25 +0530

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,68 @@
+
+print("Hello world")
+print("Hey I am a good girl\nand this viewer is also good girl")
+
+#"hey sakshi"  change it with hey mam
+print("I want to become a engineer")
+print("hey" , 6 ,7,sep="~")
+
+#"good girl" change it with only girl
+print('123*5') #give me the ans of 123*5
+
+a=12735372882920192983336
+print(a)
+a="Sakshi"
+print(a)
+
+a=380
+print(a)
+b=20
+print(a+b)
+c="sakshi"
+d=56
+
+print("the type of a is " , type(a))
+print("the type of b is " , type(b))
+print("the type of c is " , type(c))
+print("the type of d is " , type(d))
+
+print("rules foe identifiers")
+print (" scan the paper ")
+a1="kung fu karate"
+print("the type of a1 is " , type(a1))
+
+print(238+93)
+print(123-23)
+print(150*10)
+print(400/2)
+print(5//2)
+print(5**3)
+print(2746296+92638639)
+print(245*22)
+
+a="sakshi"
+b="Giri"
+print(a+b)
+a = 123
+b = 321
+print(a+b)
+
+name = "sakshi"
+price = 23.87
+age=18
+print(name)
+print(price)
+print(age)
+
+#list
+l=[2,4,6,8,10]
+print(l)
+s=("apple,cake,grapes,vegetables,cookies")
+print(s)
+print("type of s is" ,type(s))
+
+str = "apna college"
+print(str[0:4])
+print(str[0:12])
+print(str[5:12])
+print(str[5 : len(str)])
